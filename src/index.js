f4fc98a3f4d24711e225a716e10e8dbad8cceb2b
Prince Asiedu <princekofasiedu@gmail.com>
2020-08-10 02:46:56 -0700

    Began user-functionality

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import * as serviceWorker from "./serviceWorker";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faAngleDoubleRight, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faCheckSquare, faCoffee, faAngleDoubleRight)
+library.add(fab, faCheckSquare, faCoffee, faAngleDoubleRight, faUser, faBars)
 ReactDOM.render(
   <React.StrictMode>
     <App />
